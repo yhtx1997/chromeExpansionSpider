5cf90597482be550d9c3a0e29919432e2ad22b85
yhtx1997 <1442150545@qq.com>
2018-11-25 16:53:35 +0800

    add vue

---
+++ index/js/index.js
@@ -0,0 +1,9 @@
+chrome.devtools.panels.create("Spider","","./index/index.html");
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        quantity: 0,
+        info:['asdasd',465464,'asdasdasdawd']
+    }
+})
